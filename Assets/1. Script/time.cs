013ee245a4e123b43cf5ff95e629d1b5e9cb6e0f
rerong <ran5962@ajou.ac.kr>
2016-10-18 20:59:49 +0900

    일부 파일 수정 및 쓸모없는 파일 삭제

---
+++ Assets/1. Script/time.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class time : MonoBehaviour {
+
+    public UnityEngine.UI.Slider _silder;
+    public UnityEngine.UI.Button btn;
+    public FlockController flockcontroller;
+
+    private int maxBird = 25;
+
+    [Range(-100f, 100f)]
+    public float summonTime;
+
+    void Start()
+    {
+        btn.interactable = false;
+    }
+
+    void Update()
+    {
+        _silder.value += Time.deltaTime/summonTime;
+        if (_silder.value == 1)
+            btn.interactable = true;
+    }
+
+    public void clicked()
+    {
+        if (flockcontroller._childAmount < maxBird)
+            flockcontroller._childAmount += 1;
+        else
+            
+
+        _silder.value = 0;
+        btn.interactable = false;
+
+            
+    }
+}
