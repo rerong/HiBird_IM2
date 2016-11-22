98ecde81fcab593fd90fb855857affb6d19796c0
rerong <ran5962@ajou.ac.kr>
2016-11-22 21:51:51 +0900

    mountainManager 추가

---
+++ Assets/1. Script/mountainManager.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mountainManager : MonoBehaviour 
+{
+	public canvasManager canvas;
+	public UnityEngine.UI.Text lpsText;
+	public float cost;
+	public float objLPS;
+	public int objUnit;
+	public int unit;
+	public int upgrade;
+	public float costRate;
+	private string unitStr;
+	private int level = 0;
+	private loveCalc calc = new loveCalc();
+	private bool isShow;
+
+	//현진 추가
+	public GameObject mountain;
+	public GameObject mountainHeightLimit;
+
+	private Vector3 origin;
+	private Vector3 trans;
+
+	void Start() 
+	{
+		if (unit == 64)
+			unitStr = null;
+		else 
+		{
+			calc.firstChangeCalc (cost, unit);
+			unitStr = calc.returnChangeUnit ();
+		}
+		isShow = false;
+
+		// 현진 추가
+		origin = mountain.transform.position;
+		trans = mountainHeightLimit.transform.position;
+	}
+
+	void Update() 
+	{
+		if (calc.changeCalcLove (cost, unit)) 
+		{
+			cost = calc.returnChangeCost ();
+			unit = calc.returnChangeNumUnit ();
+			unitStr = calc.returnChangeUnit ();
+		}
+
+		if (isShow)
+			lpsText.text = cost.ToString ("F2") + unitStr + "\n" + (level + 1) + "level\n";
+		else
+			lpsText.text = "tree level : " + upgrade;
+	}
+
+	public void purchasedItem() 
+	{
+
+		if (isShow)
+		{
+			if (calc.compareCalc(canvas.gameManaging.getLove(), canvas.gameManaging.getLoveUnit(), cost, unit))
+			{
+				canvas.gameManaging.setLove (calc.returnChangeCost(), calc.returnChangeNumUnit(), calc.returnChangeUnit());
+				level += 1;
+				objLPS = objLPS * 1.04f;
+				cost *= costRate;
+			}
+
+			if (calc.changeCalcLove (cost, unit)) 
+			{
+				cost = calc.returnChangeCost ();
+				unit = calc.returnChangeNumUnit ();
+				unitStr = calc.returnChangeUnit ();
+			}
+
+			if (calc.changeCalcLove (objLPS, objUnit)) 
+			{
+				objLPS = calc.returnChangeCost ();
+				objUnit = calc.returnChangeNumUnit ();
+			}
+
+			if (level <= 50)
+				mountain.transform.position = Vector3.Lerp(origin, trans, level * 0.02f);
+			else if (level <= 450)
+				mountain.GetComponent<MeshRenderer>().material.color = Color.Lerp(canvas.level0.color, canvas.level2.color, (level - 50) * 0.0025f);
+		}
+	} 
+		
+	public int getLevel()
+	{
+		return level;
+	}
+
+	public bool getShow()
+	{
+		if (!isShow) 
+		{
+			if (canvas.gameManaging.getTreeLevel () > upgrade) 
+			{
+				isShow = true;
+			}
+		}
+
+		return isShow;
+	}
+
+	public float getObjLPS()
+	{
+		return objLPS;
+	}
+
+	public int getObjUnit()
+	{
+		return objUnit;
+	}
+}
