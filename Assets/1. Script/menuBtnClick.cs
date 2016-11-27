37689a2b201d0786655eed31f92279750dd59cec
HongHyunJin <kiju0164@ajou.ac.kr>
2016-11-27 22:48:59 +0900

    미프 데모 1차 버전

--- Assets/1. Script/menuBtnClick.cs
+++ Assets/1. Script/menuBtnClick.cs
@@ -4,39 +4,70 @@ using System.Collections;
 public class menuBtnClick : MonoBehaviour {
 
 	public GameObject btn;
-	public canvasManager canvas;
+
+    public Sprite change;
+    public Sprite origin;
+    public Sprite origin1;
+    public Sprite origin2;
+
+    public canvasManager canvas;
 
 	public void Start()
 	{
-	}
+        canvas.treeMenuBtn.image.overrideSprite = change;
+    }
 
 	public void Clicked()
 	{
 
 		if (btn.name.Contains("tree"))
 		{
-			canvas.treePanel.SetActive (true);
+            canvas.treeMenuBtn.image.overrideSprite = change;
+
+            canvas.mountainMeunBtn.image.overrideSprite = origin;
+            canvas.birdMenuBtn.image.overrideSprite = origin1;
+            canvas.cashMenuBtn.image.overrideSprite = origin2;
+
+            canvas.treePanel.SetActive (true);
 			canvas.mountainPanel.SetActive (false);
 			canvas.birdPanel.SetActive (false);
 			canvas.cashPanel.SetActive (false);
 		}
 		else if (btn.name.Contains("mountain"))
 		{
-			canvas.treePanel.SetActive (false);
+            canvas.mountainMeunBtn.image.overrideSprite = change;
+
+            canvas.treeMenuBtn.image.overrideSprite = origin;
+            canvas.birdMenuBtn.image.overrideSprite = origin1;
+            canvas.cashMenuBtn.image.overrideSprite = origin2;
+
+            canvas.treePanel.SetActive (false);
 			canvas.mountainPanel.SetActive (true);
 			canvas.birdPanel.SetActive (false);
 			canvas.cashPanel.SetActive (false);
 		}
 		else if (btn.name.Contains("bird"))
 		{
-			canvas.treePanel.SetActive (false);
+            canvas.birdMenuBtn.image.overrideSprite = change;
+
+            canvas.treeMenuBtn.image.overrideSprite = origin;
+            canvas.mountainMeunBtn.image.overrideSprite = origin1;
+            canvas.cashMenuBtn.image.overrideSprite = origin2;
+
+            canvas.treePanel.SetActive (false);
 			canvas.mountainPanel.SetActive (false);
 			canvas.birdPanel.SetActive (true);
 			canvas.cashPanel.SetActive (false);
 		}
 		else if (btn.name.Contains("cash"))
 		{
-			canvas.treePanel.SetActive (false);
+            canvas.cashMenuBtn.image.overrideSprite = change;
+
+            canvas.treeMenuBtn.image.overrideSprite = origin;
+            canvas.mountainMeunBtn.image.overrideSprite = origin1;
+            canvas.birdMenuBtn.image.overrideSprite = origin2;
+
+            canvas.treePanel.SetActive (false);
 			canvas.mountainPanel.SetActive (false);
 			canvas.birdPanel.SetActive (false);
 			canvas.cashPanel.SetActive (true);
