88ccf81ce118fe027696217f50d1779bf47d9365
HongHyunJin <kiju0164@ajou.ac.kr>
2017-03-11 13:08:38 +0900

    20170311 / 새로운 시작

--- Assets/1. Script/CloudsControl.cs
+++ Assets/1. Script/CloudsControl.cs
@@ -4,14 +4,11 @@ using System.Collections;
 public class CloudsControl : MonoBehaviour {
 
 	//Range for min/max values of variable
-	[Range(-100f, 100f)]
+	[Range(-10f, 10f)]
 	public float cloudsMoveSpeed_x, cloudsMoveSpeed_z;
 
 	// Clouds Movement
 	void Update () {
-        
 		gameObject.transform.Translate (cloudsMoveSpeed_x * Time.deltaTime, 0f, cloudsMoveSpeed_z * Time.deltaTime);
-       /* if(gameObject.transform.position.x > 255)
-            { gameObject.transform.Translate(-255, 0, 0); }*/
-    }
+	}
 }
